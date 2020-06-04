ed8df3c1f7666138de35a96c236edc99b0003823
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-06-04 13:29:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function hoursWorkedOnDate(date){
 }
 
 function wagesEarnedOnDate(date){
-  return this.payPerHour * hoursWorkedOnDate(date);
+  return this.payPerHour * hoursWorkedOnDate.call(this, date);
 }
 
 /*
