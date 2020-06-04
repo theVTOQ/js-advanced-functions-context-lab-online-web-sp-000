3d3dfc432bf68daff3d23ec714e829821c0379a8
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-06-04 13:42:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
 /* Your Code Here */
+function createEmployeeRecord(employee){
+  return {
+    firstName: employee[0],
+    familyName: employee[1],
+    title: employee[2],
+    payPerHour: employee[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
+
+function createEmployeeRecords(employees){
+  return employees.map(employee => createEmployeeRecord(employee));
+}
+
+function createTimeInEvent(time){
+  const splitTime = time.split(' ');
+  const date = splitTime[0];
+  const hour = parseInt(splitTime[1]);
+  this.timeInEvents.push({type: "TimeIn", date: date, hour: hour});
+  return this;
+}
+
+function createTimeOutEvent(time){
+  const splitTime = time.split(' ');
+  const date = splitTime[0];
+  const hour = parseInt(splitTime[1]);
+  this.timeOutEvents.push({type: "TimeOut", date: date, hour: hour});
+  return this;
+}
+
+function hoursWorkedOnDate(date){
+  const timeIn = this.timeInEvents.find(event => event.date == date);
+  const startingHour = parseInt(timeIn.hour);
+  const timeOut = this.timeOutEvents.find(event => event.date == date);
+  const endingHour = parseInt(timeOut.hour);
+  return (endingHour - startingHour)/100;
+}
+
+function wagesEarnedOnDate(date){
+  return this.payPerHour * hoursWorkedOnDate.call(this, date);
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -19,4 +61,16 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function payrollExpense(){
+  return this.reduce((total, employee) => total + employee.allWagesFor());
+}
+
+function findEmployeeByFirstName(employees, firstName){
+  return employees.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(employeeRecords){
+  return employeeRecords.reduce((total, employee) => total + allWagesFor.call(employee), 0);
 }
